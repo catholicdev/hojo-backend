a9de9ab7c13b165ec93dfa908f2993037cfe5beb
nhiennhatt <hiennhatt1804@gmail.com>
2022-11-10 09:31:36 +0000

    Add book-group db

--- libs/types/src/enums/book-group.enum.ts
+++ libs/types/src/enums/book-group.enum.ts
@@ -1,4 +1,11 @@
 export enum BookGroupEnum {
   NONE = "none",
   PENTATEUCH = "pentateuch",
+  HISTORY = "history",
+  WISDOM = "wisdom",
+  PROPHET = "prophet",
+  GOSPEL = "gospel",
+  HISTORICAL_APOSTLES = 'historical_apostle',
+  EPISTLES = 'epistles',
+  REVELATION = 'revelation'
 }
